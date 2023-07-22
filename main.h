09fb065ed457e127046cd13c27b218000f8cd2c2
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-07-22 01:34:10 -0700

    Printf Project

--- main.h
+++ main.h
@@ -68,15 +68,15 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 		char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function to print non printable characters */
+/* Functions to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/* Functions to print memory address */
 int print_pointer(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
-/* Funciotns to handle other specifiers */
+/* Functions to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
