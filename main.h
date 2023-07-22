09b945da941ae8c48091563f80e0e2658d30c443
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-07-22 01:37:35 -0700

    Printf Project

--- main.h
+++ main.h
@@ -43,7 +43,7 @@ int handle_print(const char *fmt, int *i,
 
 /****************** FUNCTIONS ******************/
 
-/* Funtions to print chars and strings */
+/* Functions to print chars and strings */
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -82,11 +82,11 @@ int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
-/*Function to print string in reverse*/
+/*Functions to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
-/*Function to print a string in rot 13*/
+/*Functions to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
