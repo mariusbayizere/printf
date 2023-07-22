d15066a6d07a7fa4d86f05c97d7d43910fdcc8ce
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-07-22 04:14:48 -0700

    Printf Project

--- get_precision.c
+++ get_precision.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * get_precision - function that calculates the precision for printing
+ * get_precision - function that calculates
+ * the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
@@ -14,7 +15,6 @@ int get_precision(const char *format, int *i, va_list list)
 
 	if (format[curr_i] != '.')
 		return (precision);
-
 	precision = 0;
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
